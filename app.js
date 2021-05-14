5ba8c89fbc7d1aa4a6ac2995acd2d122793d3086
mahitha09 <mahitha.yellani@gmail.com>
2021-05-14 08:16:59 -0400

    update

---
+++ app.js
@@ -0,0 +1,27 @@
+//variables
+const btn = document.querySelector('.btn');
+const textArea = document.querySelector('.textArea');
+const container = document.querySelector('.container');
+
+//Eventlisteners
+btn.addEventListener('click',(e) => {
+    e.preventDefault();
+    addNewNote();
+});
+
+//functions
+function addNewNote(){
+    const noteText = textArea.value;
+    const notes = document.createElement('div');
+    notes.classList.add('notes');
+    const htmlData = `
+    <div class="tools" id="tools">
+                    <button class="edit"><i class="far fa-edit"></i></button>
+                    <button class="delete"><i class="far fa-trash-alt"></i></button>
+                </div>
+                <div class="main">
+                    <textarea class="textArea"></textarea>
+                </div>
+    `;
+    
+}
