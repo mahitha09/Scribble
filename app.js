01ce6ed3f426d230950e47a69c4309fc3cf47274
mahitha09 <mahitha.yellani@gmail.com>
2021-05-16 17:29:04 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //variables
 const btn = document.querySelector('.btn');
-const textArea = document.querySelector('.textArea');
 const container = document.querySelector('.container');
 
 //Eventlisteners
@@ -9,18 +8,30 @@ btn.addEventListener('click',() => {
 });
 
 //functions
-function addNewNote(){
-    const noteText = textArea.value;
+function addNewNote(text){
     const notes = document.createElement('div');
     notes.classList.add('notes');
     const htmlData = `
     <div class="tools" id="tools">
-                    <button class="edit"><i class="far fa-edit"></i></button>
+                    <button id="edit"><i class="far fa-edit"></i></button>
                     <button class="delete"><i class="far fa-trash-alt"></i></button>
                 </div>
                 <div class="main">
                     <textarea class="textArea"></textarea>
                 </div>
     `;
-    
+    notes.insertAdjacentHTML('afterbegin',htmlData);
+
+const editButton = document.querySelectorAll('#edit');
+const deleteBtn = document.querySelector('.delete');
+const textArea = document.querySelector('.textArea');
+const main = document.querySelector('.main');
+console.log(editButton);
+
+editButton.addEventListener('click',display);
+function display(){
+    console.log('hii');
+}
+
+    container.appendChild(notes);
 }
