2bd13ebd9a18ab35193104fe9d6e06d46b7a465f
mahitha09 <mahitha.yellani@gmail.com>
2021-05-16 21:04:51 +0530

    Update

--- app.js
+++ app.js
@@ -2,36 +2,74 @@
 const btn = document.querySelector('.btn');
 const container = document.querySelector('.container');
 
+//Setting values to localstorage
+function updateLS(){
+    const note = document.querySelectorAll('.textArea');
+    const notesArr = [];
+    
+    note.forEach((notes)=>{
+        return notesArr.push(notes.value);
+    });
+
+    localStorage.setItem('notesArr',JSON.stringify(notesArr));
+}
+
 //Eventlisteners
 btn.addEventListener('click',() => {
     addNewNote();
 });
 
 //functions
-function addNewNote(text){
+function addNewNote(text = ''){
+
     const notes = document.createElement('div');
     notes.classList.add('notes');
+    
     const htmlData = `
     <div class="tools" id="tools">
-                    <button id="edit"><i class="far fa-edit"></i></button>
+                    <button class="edit"><i class="far fa-edit"></i></button>
                     <button class="delete"><i class="far fa-trash-alt"></i></button>
                 </div>
-                <div class="main">
-                    <textarea class="textArea"></textarea>
-                </div>
+                <div class="main ${text?'':'none'}"></div>
+                <textarea class="textArea ${text?'none':''}"></textarea>
+                
     `;
     notes.insertAdjacentHTML('afterbegin',htmlData);
 
-const editButton = document.querySelectorAll('#edit');
-const deleteBtn = document.querySelector('.delete');
-const textArea = document.querySelector('.textArea');
-const main = document.querySelector('.main');
-console.log(editButton);
+    const editBtn = notes.querySelector('.edit');
+    const deleteBtn = notes.querySelector('.delete');
+    const textArea = notes.querySelector('.textArea');
+    const main = notes.querySelector('.main');
 
-editButton.addEventListener('click',display);
-function display(){
-    console.log('hii');
+    editBtn.addEventListener('click',()=>{
+        textArea.classList.toggle('none');
+        main.classList.toggle('none');
+    });
+
+    textArea.value = text;
+    main.innerHTML = '<p>'+text+'</p>';
+
+    //activating delete button
+    deleteBtn.addEventListener('click',()=>{
+        notes.remove();
+        updateLS();
+    });
+
+    textArea.addEventListener('change', (e) => {
+        const value = e.target.value;
+        main.innerHTML = '<p>'+value+'</p>';
+        //updating localstorage on every change
+        updateLS();
+    });
+
+    //Appending items to document
+    container.appendChild(notes);
 }
 
-    container.appendChild(notes);
+//Getting values from localstorage
+const notesArr = JSON.parse(localStorage.getItem('notesArr'));
+if(notesArr){
+    notesArr.forEach((notes) => {
+        addNewNote(notes);
+    });
 }
